c56c2f0eb2cefdcbf8a7e85864c5dd63d798b01f
vboxuser <yeelacitron@gmail.com>
2023-06-11 18:59:31 +0300

    first commit

--- Demo.cpp
+++ Demo.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 #include "sources/MagicalContainer.hpp"
 
 using namespace ariel;
@@ -17,16 +18,18 @@ int main() {
     // Use AscendingIterator to display elements in ascending order
     std::cout << "Elements in ascending order:\n";
     MagicalContainer::AscendingIterator ascIter(container);
+    
     for (auto it = ascIter.begin(); it != ascIter.end(); ++it) {
+        
         std::cout << *it << ' ';   // 2 3 9 17 25
-    }
+   }
     std::cout << std::endl;
 
     // Use DescendingIterator to display elements in descending order
     std::cout << "Elements in cross order:\n";
     MagicalContainer::SideCrossIterator crossIter(container);
     for (auto it = crossIter.begin(); it != crossIter.end(); ++it) {
-        std::cout << *it << ' ';  // 2 25 3 17 9
+       std::cout << *it << ' ';  // 2 25 3 17 9
     }
     std::cout << std::endl;
 
@@ -38,9 +41,9 @@ int main() {
     }
     std::cout << std::endl;
 
-    // Remove an element from the container and display the size
-    container.removeElement(9);
-    std::cout << "Size of container after removing an element: " << container.size() << std::endl;
+    // // Remove an element from the container and display the size
+    // container.removeElement(9);
+    // std::cout << "Size of container after removing an element: " << container.size() << std::endl;
 
     return 0;
-}
+  }
--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@ CXXVERSION=c++2a
 TIDY=clang-tidy-14
 SOURCE_PATH=sources
 OBJECT_PATH=objects
-CXXFLAGS=-std=$(CXXVERSION) -Werror -Wsign-conversion -I$(SOURCE_PATH)
+CXXFLAGS=-std=$(CXXVERSION) -Werror -g -Wsign-conversion -I$(SOURCE_PATH)
 TIDY_FLAGS=-extra-arg=-std=$(CXXVERSION) -checks=bugprone-*,clang-analyzer-*,cppcoreguidelines-*,performance-*,portability-*,readability-*,-cppcoreguidelines-pro-bounds-pointer-arithmetic,-cppcoreguidelines-owning-memory --warnings-as-errors=*
 VALGRIND_FLAGS=-v --leak-check=full --show-leak-kinds=all  --error-exitcode=99
 
